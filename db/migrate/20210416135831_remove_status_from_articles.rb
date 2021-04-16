b81103169ed1278b9e3c772b79ea4f4b132ac1e7
Alex <alex96ford19@gmail.com>
2021-04-16 15:00:49 +0100

    Changed the database schema and put the project back together.

---
+++ db/migrate/20210416135831_remove_status_from_articles.rb
@@ -0,0 +1,6 @@
+class RemoveStatusFromArticles < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :articles, :status, :string
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_112119) do
+ActiveRecord::Schema.define(version: 2021_04_16_135831) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
